8b601e010df8a34540ee4d18ec5aede8ebab0b5e
xferreiraghf <143434753+xferreiraghf@users.noreply.github.com>
2023-08-29 10:55:17 -0300

    Finalizado o Mapeamento - Saneamento Cadastral
    Finalizei o mapeamento das funções da sessão tczbrc210m000. Comentei todos os pontos importantes para realizar a análise e adicionar na planilha de integrações.

--- Audicon - Mapeamento/Saneamento Cadastral - tczbrc210m000.sql
+++ Audicon - Mapeamento/Saneamento Cadastral - tczbrc210m000.sql
@@ -1,118 +1,3 @@
-|******************************************************************************
-|* tczbrc210  0  VRC B61C a7 bdl0
-|* Saneamento cadastral
-|* Developer customization 05                                                                                              
-|* 2012-08-24
-|******************************************************************************
-|* Form Type 4
-|******************************************************************************
-|* TIM.442, Tiago Mira, 2014-09-24, B61U_a_prd0
-|* Customização reescrita.
-|* CRVE096.
-|*
-|* I15197312, Wellington Almeida, 2021-03-04
-|* Filtrar PN a partir de um arquivo texto 
-|* 
-|* BDL.106196, Wellington Almeida, 2021-10-07
-|* So permitir uma execucao por vez da sessao.
-|* 
-|******************************************************************************
-                                                                             
-|****************************** declaration section ***************************
-declaration:
-#ident "@(#)TIM.442, Tiago Mira, 2014-09-24, B61U_a_prd0"
-#ident "@(#)I15197312, Wellington Almeida, 2021-03-04, B61U_a_prd0"
-#ident "@(#)BDL.106196, Wellington Almeida, 2021-10-07, B61U_a_prd0"
-
-	table	ttccom100	|* Parceiros de negócios
-	table	ttccom130	|* Addresses
-	table	ttczbr012	|* Saneamento Cadastral
-	table	ttczbr013	|* Inconsistencias Cadastrais
-	table	ttczbr014	|* Parceiro de Negócios com Restrições
-|* Cadastrais.
-	table	ttdsls400	|* Ordens de vendas
-	table	ttczbr000	|* Parametros customizados Brandili
-	
-	extern	domain	tccom.bpid		bpid.f.c	fixed
-	extern	domain	tccom.bpid		bpid.t.c	fixed
-	extern	domain	tcdate			crdt.f.c
-	extern	domain	tcdate			crdt.t.c
-	extern	domain	tcdate			stdt.f.c
-	extern	domain	tcdate			stdt.t.c
-	extern	domain	tcdate			odat.f.c
-	extern	domain	tcdate			odat.t.c
-	extern	domain	tcdate			ddat.f.c
-	extern	domain	tcdate			ddat.t.c
-	extern	domain	tcyesno			f.ejob.c
-	extern	domain	tcyesno			f.udma.c
-	extern	domain	tcyesno			f.arqu.c	|#I15197312.sn
-	extern	domain	tcmcs.str100		f.cami.c	
-			long			ret.file.c
-			string			regi.c(1000)	|#I15197312.en
-			long			i
-
-	#include "itccom0000"	|* Multicurrency variables and handling
-	#include <bic_desktop>					|#I15197312.n
-
-|****************************** program section *******************************
-before.program:
-	e = tcmcs.dll0095.read.parm("tczbr000")
-| 	e = appl.set(logname$, APPL.EXCL) 			|#BDL.106196.o
-	if application.lock.c() then 				|#BDL.106196.sn
-		exit()
-	endif							|#BDL.106196.en
-
-|****************************** group section *********************************
-group.1:
-init.group:
-	get.screen.defaults()
-
-choice.cont.process:
-on.choice:
-	clean.mess()
-	compor.tabela.para.saneamento.c()
-	if f.ejob.c = tcyesno.no then
-		start.session(MODAL, "tczbrc520m000", "prog.name$", "")
-		|* Saneamento Cadastral
-	else
-		tczbr.dllc003.processar.saneamento.cadastral.c("")
-	endif
-	mess("tccom99988", 0)
-	|* Processamento completo.
-
-|****************************** field section *********************************
-field.bpid.f.c:
-when.field.changes:
-	bpid.t.c = bpid.f.c
-
-field.crdt.f.c:
-when.field.changes:
-	crdt.t.c = crdt.f.c
-
-field.stdt.f.c:
-when.field.changes:
-	stdt.t.c = stdt.f.c
-
-field.odat.f.c:
-when.field.changes:
-	odat.t.c = odat.f.c
-
-field.ddat.f.c:
-when.field.changes:
-	ddat.t.c = ddat.f.c
-	
-field.f.arqu.c:
-when.field.changes:
-	if f.arqu.c = tcyesno.yes then 
-		enable.fields("f.cami.c")
-		disable.fields("bpid.f.c")
-		disable.fields("bpid.t.c")
-	else
-		disable.fields("f.cami.c")
-		enable.fields("bpid.f.c")
-		enable.fields("bpid.t.c")
-	endif 
-
 |****************************** function section ******************************
 functions:
 /* Funções são blocos de código que podem ser chamados em outras partes do programa para executar uma tarefa específica. */
@@ -133,47 +18,54 @@ function compor.tabela.para.saneamento.c()
 								|#BDL.106196.eo
 		select	tczbr013.* /* Inconsistencias cadastrais */
 		from	tczbr013 for update
+/* São selecionados registros da tabela tczbr013 onde o valor do campo _index1 é igual ao valor do campo bpid.c da tabela tczbr012 */		
 		where	tczbr013._index1 = { :tczbr012.bpid.c } /* Campo - Parceiro de Negócios */
 		selectdo
+/* Após a seleção, a função db.delete() é usada para excluir os registros da tabela ttczbr013. */		
 			db.delete(ttczbr013, db.retry)
 		endselect
-	
+/* São selecionados registros da tabela tczbr014 onde o valor do campo _index1 é igual ao valor do campo bpid.c da tabela tczbr012. */	
 		select	tczbr014.* /* Parceiro de negócios com Restrição Cadastral */
 		from	tczbr014 for update
 		where	tczbr014._index1 = { :tczbr012.bpid.c } /* Campo - Parceiro de Negócios */
 		selectdo
+/* A função db.delete() é usada para excluir os registros da tabela ttczbr014. */		
 			db.delete(ttczbr014, db.retry)
 		endselect
-	
+/* O campo logn.c da tabela tczbr012 é usado como argumento para a função appl.delete(), possivelmente para excluir dados de aplicação relacionados ao registro. */	
 		db.delete(ttczbr012, db.retry)
 		e = appl.delete(tczbr012.logn.c) /* Campo - Usuário */
+/*A linha |#BDL.106196.o parece indicar o final do bloco de seleção.*/
 | 		endif 						|#BDL.106196.o
 	endselect
 	commit.transaction()
-	
+/* Aqui, um indicador de progresso é criado para acompanhar o andamento do processo. O texto "Selecionando PN's para saneamento..." é exibido ao lado do indicador de progresso. */	
 	e = change.progress.indicator(0, "Selecionando PN's para saneamento...")
 	
+/* Esta parte do código verifica se a variável f.arqu.c é igual a tcyesno.yes. Se isso for verdadeiro, ele entra no bloco de código logo abaixo. Caso contrário, ele pula para o bloco dentro do else. */	
 	if f.arqu.c = tcyesno.yes then 				|#I15197312.sn
+/* Se a condição acima for verdadeira, ele chama uma função chamada abre.arquivo.c com um argumento l.data.c. Essa função abre um arquivo, possivelmente relacionado a dados de algum tipo. */	
 		abre.arquivo.c(l.data.c)
 	else
-	/* Se a condição anterior não for atendida, a função executar.saneamento.c é chamada com três argumentos: bpid.f.c, bpid.t.c, e l.data.c. */
+/* Caso a condição do passo 2 não seja atendida, ele chama uma função chamada executar.saneamento.c com três argumentos: bpid.f.c, bpid.t.c e l.data.c. Isso indica que o programa está executando algum processo de saneamento com base nos valores desses argumentos. */
 		executar.saneamento.c(
 				bpid.f.c,
 				bpid.t.c, 
 				l.data.c)
 	endif							|#I15197312.en
-	
+/* Este é um ponto de retorno que está relacionado à recuperação de dados em caso de falha durante a execução do código. */	
 | 	db.retry.point()					|#I15197312.so
 | 	select	tccom100.* /* Parceiros de Negócios */
 | 	from	tccom100,
 | 		tccom130
-| 	where	tccom100._index1 inrange { :bpid.f.c } and { :bpid.t.c } /* Campo -  Parceiro de negócios */
-| 	and	tccom100.crdt    inrange { :crdt.f.c } and { :crdt.t.c } /* Campo - data criação */
-| 	and	tccom100.stdt    inrange { :stdt.f.c } and { :stdt.t.c } /* Campo - data inicial */
-| 	and	tccom100.prst <> tccom.prst.cancel /* Campo - Status parceiro de negócios */
-| 	and	tccom130._index1 = { tccom100.cadr } /* Campo - Código de endereço */
-| 	and	tccom130.cste <> "EX"
+| 	where	tccom100._index1 inrange { :bpid.f.c } and { :bpid.t.c } /* Campo - Parceiro de negócios | tccom100._index1 deve estar dentro do intervalo especificado por bpid.f.c (valor mínimo) e bpid.t.c (valor máximo).*/
+| 	and	tccom100.crdt    inrange { :crdt.f.c } and { :crdt.t.c } /* Campo - data criação | tccom100.crdt deve estar dentro do intervalo especificado por crdt.f.c (valor mínimo) e crdt.t.c (valor máximo) */
+| 	and	tccom100.stdt    inrange { :stdt.f.c } and { :stdt.t.c } /* Campo - data inicial | tccom100.stdt deve estar dentro do intervalo especificado por stdt.f.c (valor mínimo) e stdt.t.c (valor máximo).*/
+| 	and	tccom100.prst <> tccom.prst.cancel /* Campo - Status parceiro de negócios | tccom100.prst não deve ser igual a tccom.prst.cancel.*/
+| 	and	tccom130._index1 = { tccom100.cadr } /* Campo - Código de endereço | tccom130._index1 deve ser igual a tccom100.cadr.*/
+| 	and	tccom130.cste <> "EX" /* tccom130.cste não deve ser igual a "EX" */
 | 	selectdo
+/* Os resultados são filtrados com base em datas de últimas consultas (arfb.c, astg.c e asuf.c) em relação à variável l.data.c. Se todas as três datas de consulta (arfb.c, astg.c e asuf.c) forem maiores que l.data.c, o loop continua, ignorando o restante do bloco. */
 | 		if tccom100.arfb.c > l.data.c and /* Campo - última consulta RFB */
 | 		   tccom100.astg.c > l.data.c and /* Campo - última consulta Sintegra */
 | 		   tccom100.asuf.c > l.data.c then /* Campo - última consulta Suframa */
@@ -184,18 +76,21 @@ function compor.tabela.para.saneamento.c()
 | 		from	tdsls400
 | 		where	tdsls400._index2 = { :tccom100.bpid } /* Campo - Parceiro de negócios */
 | 		selectdo
+/* Se odat.f.c não for igual a zero, o código verifica se a data de ordem tdsls400.odat está fora do intervalo especificado. Se estiver, o loop continua, ignorando o restante do bloco. */
 | 			if odat.f.c <> 0 then
 | 				if tdsls400.odat < odat.f.c or /* Campo -  Data da ordem*/
 | 				   tdsls400.odat > odat.t.c then 
 | 					continue
 | 				endif
 | 			endif
+/* Se ddat.f.c não for igual a zero, o código verifica se a data de entrega planejada tdsls400.ddat está fora do intervalo especificado. Se estiver, o loop continua, ignorando o restante do bloco. */
 | 			if ddat.f.c <> 0 then
 | 				if tdsls400.ddat < ddat.f.c or /* Campo - data de entrega planejada */
 | 				   tdsls400.ddat > ddat.t.c then 
 | 					continue
 | 				endif
 | 			endif
+/* Em seguida, a função tczbr.dllc003.saneamento.necessario.c é chamada com três parâmetros: tccom100.bpid, l.data.c e f.udma.c. */
 | 			tczbr.dllc003.saneamento.necessario.c(tccom100.bpid, /* Campo - Parceiro de negócios */
 | 							      l.data.c,
 | 							      f.udma.c)
@@ -211,18 +106,20 @@ function compor.tabela.para.saneamento.c()
 | 				i = i + 1
 | 			endif
 | 		endselect
-| 	
+|/* Se o valor de i (provavelmente um contador) atingir 1000, uma transação é confirmada e o contador é reiniciado. Isso pode estar relacionado ao controle de transações no banco de dados. */ 	
 | 		if i > 1000 then
 | 			commit.transaction()
 | 			i = 0
 | 		endif
 | 	endselect
+/* Ao final da seleção e processamento, uma última transação é confirmada e o indicador de progresso é destruído. */
 | 	commit.transaction()					|#I15197312.eo
 	
 	destroy.progress.indicator()
 }
 
 function executar.saneamento.c(					|#I15197312.sn
+/*Recebe três parâmetros: i.bpid.f.c, i.bpid.t.c e i.data.c. */
 				domain	 tccom.bpid	i.bpid.f.c, /* Campo - Parceiro de negócios */
 				domain	 tccom.bpid	i.bpid.t.c,
 				domain	tfgld.date	i.data.c)
@@ -231,13 +128,15 @@ function executar.saneamento.c(					|#I15197312.sn
 	select	tccom100.*  /* Parceiro de negócios */
 	from	tccom100,
 		tccom130
-	where	tccom100._index1 inrange { :i.bpid.f.c } and { :i.bpid.t.c }
+/*Os dados da tabela tccom100 (Parceiro de Negócios) são selecionados, possivelmente com base nos intervalos de i.bpid.f.c, i.bpid.t.c, crdt, stdt e status de parceiro (prst). */
+	where	tccom100._index1 inrange { :i.bpid.f.c } and { :i.bpid.t.c } /* tccom100._index1 deve estar dentro do intervalo especificado por bpid.f.c (valor mínimo) e bpid.t.c (valor máximo). */
 	and	tccom100.crdt    inrange { :crdt.f.c } and { :crdt.t.c } /* Campo -  Data criação */
-	and	tccom100.stdt    inrange { :stdt.f.c } and { :stdt.t.c } /* Campo - Data inicial */
-	and	tccom100.prst <> tccom.prst.cancel /* Campo - Status parceiro de negócios */
-	and	tccom130._index1 = { tccom100.cadr } /* Campo - Código de endereço */
-	and	tccom130.cste <> "EX" /* Campo - Estado/Província */
+	and	tccom100.stdt    inrange { :stdt.f.c } and { :stdt.t.c } /* Campo - Data inicial | tccom100.stdt deve estar dentro do intervalo especificado por stdt.f.c (valor mínimo) e stdt.t.c (valor máximo).*/
+	and	tccom100.prst <> tccom.prst.cancel /* Campo - Status parceiro de negócios | tccom100.prst não deve ser igual a tccom.prst.cancel.*/
+	and	tccom130._index1 = { tccom100.cadr } /* Campo - Código de endereço | deve ser igual a tccom100.cadr.*/
+	and	tccom130.cste <> "EX" /* Campo - Estado/Província  não deve ser igual a "EX"*/
 	selectdo
+/*Registros são filtrados com base em datas de últimas consultas (arfb.c, astg.c e asuf.c) em relação à variável i.data.c. Se todas as consultas forem mais recentes do que i.data.c, o loop continua para o próximo registro.*/	
 		if tccom100.arfb.c > i.data.c and
 		   tccom100.astg.c > i.data.c and
 		   tccom100.asuf.c > i.data.c then
@@ -247,23 +146,28 @@ function executar.saneamento.c(					|#I15197312.sn
 		select	tdsls400.*  /* Ordens de venda */
 | 		from	tdsls400
 		from	tdsls400
+/* Aqui, a consulta está filtrando os resultados para incluir apenas as ordens de venda que tenham um campo chamado _index2 igual ao valor de tccom100.bpid. */		
 		where	tdsls400._index2 = { :tccom100.bpid } /* Campo - Parceiro de negócios */
 		selectdo 
+/* a consulta verifica se o valor do campo odat.f.c é diferente de zero. Se for, ela compara o valor do campo tdsls400.odat (data da ordem de venda) com os valores de intervalo definidos por odat.f.c e odat.t.c. Se a data da ordem não estiver dentro desse intervalo, a instrução continue pula para a próxima iteração do loop. */		
 			if odat.f.c <> 0 then
 				if tdsls400.odat < odat.f.c or /* Campo -  Data da ordem*/
 				   tdsls400.odat > odat.t.c then
 					continue
 				endif
 			endif
+/* Esta parte é semelhante à anterior, mas para o campo ddat (data de entrega planejada).*/		
 			if ddat.f.c <> 0 then
 				if tdsls400.ddat < ddat.f.c or /* Campo - data de entrega planejada */
 				   tdsls400.ddat > ddat.t.c then
 					continue
 				endif
 			endif
+/* Se as datas estiverem dentro dos intervalos definidos, essa função é chamada com três argumentos: tccom100.bpid, i.data.c e f.udma.c. */		
 			tczbr.dllc003.saneamento.necessario.c(tccom100.bpid,
 							      i.data.c,
 							      f.udma.c)
+/* Um contador i é incrementado.*/								  
 			i = i + 1
 			break
 		selectempty
@@ -276,34 +180,36 @@ function executar.saneamento.c(					|#I15197312.sn
 				i = i + 1
 			endif
 		endselect
-	
+/* Se o contador i atingir ou ultrapassar 1000, uma transação é confirmada (comitada) e o contador i é resetado para zero. */	
 		if i > 1000 then
 			commit.transaction()
 			i = 0
 		endif
 	endselect
+/* Após o loop, uma última transação é confirmada. */	
 	commit.transaction()
 }
-
+/* Essa parte é responsável pela consulta através de arquivos*/
 function abre.arquivo.c(	domain	tfgld.date	i.data.c)
 {
 	domain 	tccom.bpid	l.bpid.c
-	
+/* O código verifica se há espaços em branco no nome do arquivo f.cami.c. Se houver espaços em branco, exibe uma mensagem de "Arquivo inválido." e encerra a função. */	
 	if isspace(f.cami.c) then
 		message("Arquivo inválido.")
 		return
 	endif	
-
+/* A função tenta transferir o conteúdo do arquivo f.cami.c para um arquivo temporário chamado "./arquivo.tmp" no servidor. Se a transferência for bem-sucedida, ret.file.c (um valor de retorno da função de transferência) será maior ou igual a 0. */	
 	ret.file.c = client2server(f.cami.c, "./arquivo.tmp", false)
-	
 	if ret.file.c < 0 then
 		message("Erro ao transferir arquivo para o servidor.")
 		return
 	endif
-	
+/* O código tenta abrir o arquivo temporário "./arquivo.tmp" para leitura. Se a abertura for bem-sucedida, ele entra em um loop while para ler o conteúdo do arquivo linha por linha. */	
 	ret.file.c = seq.open("./arquivo.tmp", "r")
-	
+
+/* A função seq.gets é usada para ler até 200 caracteres da linha atual do arquivo  */	
 	while seq.gets(regi.c, 200, ret.file.c) = 0
+/* A linha lida do arquivo (regi.c) é analisada usando string.scan para extrair um valor que é armazenado em l.bpid.c (provavelmente algum tipo de identificação). Em seguida, uma função chamada executar.saneamento.c é chamada, passando três argumentos: l.bpid.c, l.bpid.c e i.data.c. */	
 		string.scan(regi.c, "%s", l.bpid.c)
 			executar.saneamento.c(
 					l.bpid.c,
@@ -311,16 +217,19 @@ function abre.arquivo.c(	domain	tfgld.date	i.data.c)
 					i.data.c)
 	endwhile
 }								|#I15197312.en
-
+/* A função começa verificando se a chamada appl.set(prog.name$, APPL.EXCL) é diferente de 0. Isso parece estar relacionado a tentar configurar a aplicação em modo exclusivo (bloqueio). */
 function boolean application.lock.c()				|#BDL.106196.sn
 {
 	domain	tclogn 	l.user 
-	
+/* Dentro do bloco if, a função appl.get.user(prog.name$, l.user) é chamada. Usado para obter informações sobre o usuário que está utilizando a aplicação. */	
 	if appl.set(prog.name$, APPL.EXCL) <> 0 then
 		appl.get.user(prog.name$, l.user)
+/* Após obter as informações do usuário, uma mensagem é exibida utilizando o nome do usuário e a função trim$() para remover espaços em branco. */		
 		message("O usuário " & trim$(l.user) & " esta utilizando a sessão", 1)
+/* A função então retorna true, indicando que a aplicação está bloqueada porque alguém a está utilizando. */		
 		return(true)
 	endif
+/* Se a configuração da aplicação foi bem-sucedida (retorno igual a 0), o código chega ao ponto de retorno final da função e retorna false, indicando que a aplicação não está bloqueada. */
 	return(false)
 }								|#BDL.106196.en
 
