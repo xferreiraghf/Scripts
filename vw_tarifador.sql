d42fbaa1d5829bfc0e0f36fc79991fb91b08d97d
Gustavo Henrique Ferreira <143434753+xferreiraghf@users.noreply.github.com>
2024-07-12 21:55:54 -0300

    Update vw_tarifador.sql

--- vw_tarifador.sql
+++ vw_tarifador.sql
@@ -6,6 +6,7 @@ WITH tarifador_principal_ranked AS (
         tp."NumNF",
         tp."MesRef",
         tp."telefones",
+        tp."quantidade_telefone",
         tf.cnpj,
         tf.fatura,
         tp."Valor_Total",
@@ -25,13 +26,15 @@ SELECT
     "NumNF",
     "MesRef",
     "telefones",
+    "quantidade_telefone",
     cnpj,
     fatura,
     CASE WHEN SUM("Valor_Total") <> 0 THEN SUM("Valor_Total") ELSE NULL END AS total_valor,
     MAX(CASE WHEN row_num = 1 THEN valor_fatura_com_imposto ELSE NULL END) AS valor_fatura_com_imposto,
     MAX(CASE WHEN row_num = 1 THEN valor_nf_sem_imposto ELSE NULL END) AS valor_nf_sem_imposto,
     centro_custo,
-    valor_nf_sem_imposto / centro_custo_count AS valor_centro_custo, -- Divide o valor_nf_sem_imposto pelo número de centro_custo
+    valor_nf_sem_imposto / SUM("quantidade_telefone") OVER (PARTITION BY "NumNF") AS valor_por_linha, -- Calcula valor_nf_sem_imposto dividido pela quantidade_telefone por NumNF
+    (valor_nf_sem_imposto / SUM("quantidade_telefone") OVER (PARTITION BY "NumNF")) * "quantidade_telefone" AS valor_por_centro_custo, -- Calcula valor_por_centro_custo
     CASE WHEN cnpj IN ('40.432.544/0191-66', '40.432.544/0101-00') THEN -1 * MAX(CASE WHEN row_num = 1 THEN valor_nf_sem_imposto ELSE NULL END) ELSE MAX(CASE WHEN row_num = 1 THEN valor_fatura_com_imposto ELSE NULL END) - MAX(CASE WHEN row_num = 1 THEN valor_nf_sem_imposto ELSE NULL END) END AS valor_lancamento
 FROM 
     tarifador_principal_ranked
@@ -39,9 +42,10 @@ GROUP BY
     "NumNF",
     "MesRef",
     "telefones",
+    "quantidade_telefone",
     cnpj,
     fatura,
     centro_custo,
-    centro_custo_count
+    valor_nf_sem_imposto
 HAVING 
     CASE WHEN SUM("Valor_Total") <> 0 THEN SUM("Valor_Total") ELSE NULL END IS NOT NULL;
